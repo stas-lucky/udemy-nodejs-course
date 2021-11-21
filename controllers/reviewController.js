a821b681317b7d7573c83ed6fdee603740bc0c65
Stanislav Romashov <per_mail@ua.fm>
2021-11-21 23:41:23 +0100

    reviews routing

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,7 +1,7 @@
 const Review = require("./../models/reviewModel");
 const catchAsync = require("./../utils/catchAsync");
 
-exports.getAllReview = catchAsync(async (req, res, next) => {
+exports.getAllReviews = catchAsync(async (req, res, next) => {
   const reviews = await Review.find();
   res.status(200).json({
     status: "success",
@@ -13,6 +13,10 @@ exports.getAllReview = catchAsync(async (req, res, next) => {
 });
 
 exports.createReview = catchAsync(async (req, res, next) => {
+  // Allow nested routes
+  if (!req.body.tour) req.body.tour = req.params.tourId;
+  if (!req.body.user) req.body.user = req.user.id; // Corrent user
+
   const newReview = await Review.create(req.body);
   res.status(201).json({
     status: "success",
--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -2,10 +2,14 @@ const express = require("express");
 const reviewController = require("../controllers/reviewController");
 const authController = require("./../controllers/authController");
 
-const router = express.Router();
+const router = express.Router({ mergeParams: true });
 router
   .route("/")
-  .get(reviewController.getAllReview)
-  .post(authController.protect, reviewController.createReview);
+  .get(reviewController.getAllReviews)
+  .post(
+    authController.protect,
+    authController.restrictTo("user"),
+    reviewController.createReview
+  );
 
 module.exports = router;
